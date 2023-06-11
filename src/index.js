06fbf8c3f4a7ad4933b396bdf099f98a732f0992
Joseph Beltran <profbelts@gmail.com>
2023-06-12 02:06:12 +0800

    Updated index.html and index.js, deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,100 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const skills = [
+  {
+    skill: "JavaScript",
+    level: "Advanced",
+    color: "green",
+  },
+  {
+    skill: "HTML/CSS",
+    level: "Intermediate",
+    color: "yellow",
+  },
+  {
+    skill: "React",
+    level: "Beginner",
+    color: "blue",
+  },
+  {
+    skill: "NodeJS",
+    level: "Intermediate",
+    color: "yellow",
+  },
+  {
+    skill: "jQuery",
+    level: "Advanced",
+    color: "green",
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar photoName="pictures/avatar.jpg" name="profile-picture" />
+
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <div>
+      <img src={props.photoName} alt={props.name} className="avatar" />
+    </div>
+  );
+}
+
+function Intro(props) {
+  return (
+    <div>
+      <div>
+        <h1> Joseph Beltran</h1>
+      </div>
+      <div>
+        <p>
+          {" "}
+          An English Major with a passion in programming, especially in Web
+          Development. I am also a musician.{" "}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill mySkill={skill} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ mySkill }) {
+  return (
+    <div className="skill" style={{ backgroundColor: mySkill.color }}>
+      <span>{mySkill.skill}</span>
+      <span>
+        {mySkill.level === "Beginner" && "👶"}
+        {mySkill.level === "Intermediate" && "👍"}
+        {mySkill.level === "Advanced" && "💪"}
+      </span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
